62f19222d6fd9e39d6a7ca4e448412a56136c7f9
Rohan <adada.rohan@gmail.com>
2024-07-10 20:15:47 -0700

    selenium fixes

--- scraper.py
+++ scraper.py
@@ -35,30 +35,36 @@ def get_car_data(base_url:str) :
     links = []
     output_list = []
 
+    print(len(results))
+
     for result in results :
         link_element = result.find("a", {"data-cmp" : "link"}, href=True)
         if isinstance(link_element, element.Tag) and link_element['href'][0] == "/":
             links.append(link_element['href'])
 
-        # Initialize Chrome WebDriver
-    with webdriver.Chrome(service=service, options=chrome_options) as browser:
-        for link in links:
-            browser.get(f"https://www.autotrader.com{link}")
+    print(len(links))
+    # Initialize Chrome WebDriver
+    for link in links:
+        try : 
+            with webdriver.Chrome(service=service, options=chrome_options) as browser:
 
-            html = browser.page_source
-            soup = BeautifulSoup(html, "html.parser")
-            sleep(30)
-            title = soup.find("h1", {"data-cmp" : "heading"}).text
-            pricing = soup.find("span", {"data-cmp" : "firstPrice"}).text
-            feature_list = []
-            features = soup.find("ul", {"data-cmp" : "listColumns"})
-            if isinstance(features, element.Tag) : 
-                for feature in features.find_all(recursive=False):
-                    feature_list.append(feature.text)
+                browser.get(f"https://www.autotrader.com{link}")
+                html = browser.page_source
+                soup = BeautifulSoup(html, "html.parser")
+                sleep(30)
+                title = soup.find("h1", {"data-cmp" : "heading"}).text
+                pricing = soup.find("span", {"data-cmp" : "firstPrice"}).text
+                feature_list = []
+                features = soup.find("ul", {"data-cmp" : "listColumns"})
+                if isinstance(features, element.Tag) : 
+                    for feature in features.find_all(recursive=False):
+                        feature_list.append(feature.text)
 
-            description = str(soup.find("div", {"data-cmp" : "seeMore"}))
-            output_list.append(f"Title: {title} \nPrice: {pricing} \nFeatures: {feature_list} \nDescription: {description} \n")
-            print(output_list)
-        return output_list
+                description = str(soup.find("div", {"data-cmp" : "seeMore"}))
+                output_list.append(f"Title: {title} \nPrice: {pricing} \nFeatures: {feature_list} \nDescription: {description} \n")
+                print(output_list)
+        except Exception as e :
+            print(f"Error {e}")
+            break
 
-print(get_car_data("https://www.autotrader.com/cars-for-sale/all-cars/bmw/santa-clara-ca?zip=95050"))
+    return output_list
