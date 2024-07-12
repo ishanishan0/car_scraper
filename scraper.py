8f7ad64890b204230768f3e396d859796258438e
khoslsx <khosla.samir@outlook.com>
2024-07-11 21:31:00 -0700

    edge broswer

--- scraper.py
+++ scraper.py
@@ -11,19 +11,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 # Define Chrome options
-chrome_options = Options()
+# chrome_options = Options()
 #chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
+# chrome_options.add_argument("--no-sandbox")
 # Add more options here if needed
 
 # Define paths
-user_home_dir = os.path.expanduser("~")
-chrome_binary_path = os.path.join(user_home_dir, "chrome-linux64", "chrome")
-chromedriver_path = os.path.join(user_home_dir, "chromedriver-linux64", "chromedriver")
+# user_home_dir = os.path.expanduser("~")
+# chrome_binary_path = os.path.join(user_home_dir, "chrome-linux64", "chrome")
+# chromedriver_path = os.path.join(user_home_dir, "chromedriver-linux64", "chromedriver")
 
 # Set binary location and service
-chrome_options.binary_location = chrome_binary_path
-service = Service(chromedriver_path)
+# chrome_options.binary_location = chrome_binary_path
+# service = Service(chromedriver_path)
 
 
 def get_car_data(base_url:str) :
@@ -46,7 +46,7 @@ def get_car_data(base_url:str) :
     # Initialize Chrome WebDriver
     for link in links:
         try : 
-            with webdriver.Chrome(service=service, options=chrome_options) as browser:
+            with webdriver.Edge() as browser:
 
                 browser.get(f"https://www.autotrader.com{link}")
                 html = browser.page_source
@@ -68,3 +68,6 @@ def get_car_data(base_url:str) :
             break
 
     return output_list
+
+
+get_car_data("https://www.autotrader.com/cars-for-sale/all-cars/cars-under-70000/2021/porsche/taycan/cupertino-ca?newSearch=true&searchRadius=0&sortBy=derivedpriceASC&zip=95014")
